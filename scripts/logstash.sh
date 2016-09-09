1d7af04c98ad4c66a5023bf4721df433b86e2ba7
Jacob Benny John <jacobben85@gmail.com>
2016-09-09 10:10:43 -0700

    Adding sample logstash conf

--- scripts/logstash.sh
+++ scripts/logstash.sh
@@ -5,3 +5,33 @@ echo 'deb http://packages.elastic.co/logstash/2.2/debian stable main' | tee /etc
 apt-get update
 
 apt-get -y install logstash
+
+cat <<CONF >> /etc/logstash/conf.d/sample-pipeline.conf
+input {
+    file {
+        path => "/vagrant/sample-data/access_log"
+        start_position => beginning
+        ignore_older => 0
+    }
+}
+filter {
+    grok {
+        match => { "message" => "%{COMBINEDAPACHELOG}"}
+    }
+    geoip {
+        source => "clientip"
+    }
+}
+output {
+    elasticsearch {
+        hosts => [ "localhost:9200" ]
+    }
+}
+CONF
+
+echo "Running a config test"
+/opt/logstash/bin/logstash -f /etc/logstash/conf.d/sample-pipeline.conf --configtest
+
+service logstash restart
+update-rc.d logstash defaults
+update-rc.d logstash enable
